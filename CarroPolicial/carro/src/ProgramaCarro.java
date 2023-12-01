b0c0012fc9f2eb04e08741ab926c42e444b3d0ae
jonhv20192231 <61095954+jonhv20192231@users.noreply.github.com>
2023-11-30 22:06:18 -0300

    Add files via upload
    Revisão do código e implementação completada. A main agora está pegando todos os métodos das classes e respeitando a hierarquia das classes.

--- CarroPolicial/carro/src/ProgramaCarro.java
+++ CarroPolicial/carro/src/ProgramaCarro.java
@@ -1,5 +1,7 @@
+import classes.Camera;
 import classes.CarroPolicial;
 import classes.ComputadorPolicial;
+import classes.Sirene;
 
 public class ProgramaCarro {
     public static void main(String[] args) throws Exception {
@@ -7,11 +9,21 @@ public class ProgramaCarro {
 
         CarroPolicial meuCarro = new CarroPolicial();
         ComputadorPolicial meuComputador = new ComputadorPolicial("PC Policial 2000", "123456", "Windows");
+        
+        
 
         // Agora você pode usar os métodos dos objetos criados
         meuCarro.acelerar(50); // O carro está agora a 50 km/h.
         meuComputador.ligar();
         meuComputador.verificarSeguranca();
         meuComputador.desligar();
+      
+        Sirene minhaLampada = new Sirene();
+        minhaLampada.acender(); // A sirene esta apitando.
+        minhaLampada.apagar();  // A sirene parou de apitar.
+
+        Camera minhaCamera = new Camera();
+        minhaCamera.tirarFoto();
+        
     }
 }
